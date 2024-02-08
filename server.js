da012accf7f54ae202fea0e80d34c676b1b1088f
parkyotinpanta <khwansingz@hotmail.com>
2024-02-08 15:27:32 +0700

    diragram

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
-
+const knex = require('./connet.knex')
 
 const app = express()
 
 
-
+app.get('/provines', async (req, res) => {
+    const park = await knex.select().table('provinces')
+    console.log(park)
+    return res.status(200).json(park)
+})
 
 
 
